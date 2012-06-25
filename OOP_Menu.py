17ad9c8f91b43814d6b49958a00833b5d6e4e6f2
elvirich04 <ma.elvira.abellera@gmail.com>
2012-06-25 10:07:50 +0800

    OOP  Menu2

--- OOP_Menu.py
+++ OOP_Menu.py
@@ -2,3 +2,17 @@ class MenuItem:
     def __init__ (self):
         self.name=""
         self.price=0
+    def Show(self):#def Show (class method)
+        print("%s        %i"%(self.name, self.price))
+        
+Spag=MenuItem()#data type: Menu Item (instance)
+Spag.name="Spaghetti"
+Spag.price=95#price (class member)
+
+Coke=MenuItem()
+Coke.name="Coke Float"
+Coke.price=25
+
+Spag.Show()
+Coke.Show()
+
