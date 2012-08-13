472073d79b4e062e67071931c278c734154a0043
Ma. Elvira Abellera Oco <ma.elvira.abellera@gmail.com>
2012-08-13 10:01:09 +0800

    excercise Text App

---
+++ Text_app3.py
@@ -0,0 +1,39 @@
+import tkinter
+class Application(tkinter.Frame):
+    def __init__ (self,master):
+        tkinter.Frame.__init__(self,master)
+        self.pack()
+        self.CreateWidgets()
+    def CreateWidgets(self):
+        self.textbox1=tkinter.Text()
+        self.textbox1.pack()
+        self.button1=tkinter.Button()
+        self.button1["command"]=self.button1_click
+        self.button1["text"]="click"
+        self.button1.pack()
+
+        self.button2=tkinter.Button()
+        self.button2["command"]=self.button1_save
+        self.button2["text"]="save"
+        self.button2.pack()
+
+        self.button3=tkinter.Button()
+        self.button3["command"]=self.button1_load
+        self.button3["text"]="load"
+        self.button3.pack()
+        
+    def button1_click(self):
+        text=self.textbox1.get(0.0,tkinter.END)
+        print(text.upper())
+        print(text.lower())
+        print(text.title())
+
+    def button1_save(self):
+        pass
+    def button1_load(self):
+        pass
+    
+root=tkinter.Tk()
+app=Application(master=root)
+app.mainloop()
+root.destroy()
